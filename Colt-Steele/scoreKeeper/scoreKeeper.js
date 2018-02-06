ed8d4dd2478d30c19d48cbd97e68c585743ad7c6
Carmen Peralta <cperalta26@gmail.com>
2018-02-06 14:34:20 -0500

    add game over functionality

--- Colt-Steele/scoreKeeper/scoreKeeper.js
+++ Colt-Steele/scoreKeeper/scoreKeeper.js
@@ -6,12 +6,25 @@ const p2Button = document.getElementById("p2")
 const p2Display = document.getElementById("p2Display")
 let p2Score = 0
 
+let gameOver = false
+const winningScore = 5
+
 p1Button.addEventListener("click", ()=>{
-  p1Score++
-  p1Display.innerText = p1Score
+  if (!gameOver) {
+    p1Score++
+    if (p1Score === winningScore) {
+      gameOver = true
+    }
+    p1Display.innerText = p1Score
+  }
 })
 
 p2Button.addEventListener("click", ()=>{
-  p2Score++
-  p2Display.textContent = p2Score
+  if (!gameOver) {
+    p2Score++
+    if (p2Score === winningScore) {
+      gameOver = true
+    }
+    p2Display.textContent = p2Score
+  }
 })
