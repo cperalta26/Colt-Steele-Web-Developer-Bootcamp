3fe473b4e4f26adb89097a1ba5853a2341fd8092
Carmen Peralta <cperalta26@gmail.com>
2018-02-06 13:50:10 -0500

    add player2 scoreboard functionality

--- Colt-Steele/scoreKeeper/scoreKeeper.js
+++ Colt-Steele/scoreKeeper/scoreKeeper.js
@@ -2,8 +2,16 @@ const p1Button = document.querySelector("#p1")
 const p1Display = document.querySelector("#p1Display")
 let p1Score = 0
 
+const p2Button = document.getElementById("p2")
+const p2Display = document.getElementById("p2Display")
+let p2Score = 0
 
 p1Button.addEventListener("click", ()=>{
-  p1Display.innerText = p1Score
   p1Score++
+  p1Display.innerText = p1Score
+})
+
+p2Button.addEventListener("click", ()=>{
+  p2Score++
+  p2Display.textContent = p2Score
 })
