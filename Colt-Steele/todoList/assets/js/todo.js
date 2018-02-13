c12a7cdbe89cb9155966c9557904e5363c40c805
Carmen Peralta <cperalta26@gmail.com>
2018-02-12 21:32:55 -0500

    add todos to list

--- Colt-Steele/todoList/assets/js/todo.js
+++ Colt-Steele/todoList/assets/js/todo.js
@@ -1,12 +1,21 @@
 //check off Specific Todos By Clicking
-$('li').click(function(){
+$('ul').on('click', 'li', function(){
  $(this).toggleClass('completed')
 })
 
 //Click on X to delete Todo
-$('span').click(function(event){
+$('ul').on('click', 'span', function(event){
   $(this).parent().fadeOut(500, function() {
     $(this).remove()
   })
   event.stopPropagation()
+})
+
+//Add a Todo
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    const todoText = $(this).val()
+    $(this).val('')
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+  }
 })
