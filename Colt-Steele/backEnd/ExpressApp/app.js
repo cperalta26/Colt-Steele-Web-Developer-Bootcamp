1d80ecfed58dfbeb80911a940016b8b835f7d65d
Carmen Peralta <cperalta26@gmail.com>
2018-02-27 19:54:18 -0500

    write routes using route params

--- Colt-Steele/backEnd/ExpressApp/app.js
+++ Colt-Steele/backEnd/ExpressApp/app.js
@@ -17,6 +17,19 @@ app.get('/dog', (req, res, next) => {
   res.send('MEOW!!!!')
 })
 
+app.get('/r/:subredditName', (req, res) => {
+  const subreddit = req.params.subredditName
+  res.send(`WELCOME TO THE ${subreddit.toUpperCase()} SUBREDDIT!`)
+})
+
+app.get('r/:subredditName/comments/:id/:title', (req, res) => {
+  res.send('Welcome to the comments page!!!')
+})
+
+app.get('*', (req, res, next) => {
+  res.send('You Are a Star!')
+})
+
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
