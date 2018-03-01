db17f21a7ce651f1e8bd7aa9c55493714ca5bcb6
Carmen Peralta <cperalta26@gmail.com>
2018-03-01 11:39:09 -0500

    add loops

--- Colt-Steele/backEnd/moreExpress/EJSDemo/app.js
+++ Colt-Steele/backEnd/moreExpress/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get('/fallinlovewith/:thing', (req, res) => {
   res.render('love.ejs', {thing})
 })
 
+app.get('/posts', (req, res, next) => {
+  const posts = [
+    {title: 'Post 1', author: 'Susy'},
+    {title: 'My adorable pet bunny', author: 'Charlie'},
+    {title: 'Can you believe this pomsky?', author: 'Colt'}
+  ]
+
+  res.render('posts.ejs', {posts})
+})
+
 app.listen(3000, () => {
   console.log('Server is listening on PORT 3000!!!')
 })
