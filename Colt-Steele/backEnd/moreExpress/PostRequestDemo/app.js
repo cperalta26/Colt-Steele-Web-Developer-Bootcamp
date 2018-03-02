b2607b4a791ed776c973294037f6358734adc591
Carmen Peralta <cperalta26@gmail.com>
2018-03-01 19:03:58 -0500

    add the add a friend route

--- Colt-Steele/backEnd/moreExpress/PostRequestDemo/app.js
+++ Colt-Steele/backEnd/moreExpress/PostRequestDemo/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+const friends = ['Tony', 'Miranda', 'Justin', 'Pierre', 'Lily']
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs')
 
@@ -7,8 +12,13 @@ app.get('/', (req, res, next) => {
   res.render('home')
 })
 
+app.post('/addfriend', (req, res) => {
+  const newFriend = req.body.newfriend
+  friends.push(newFriend)
+  res.redirect('/friends')
+})
+
 app.get('/friends', (req, res) => {
-  const friends = ['Tony', 'Miranda', 'Justin', 'Pierre', 'Lily']
 
   res.render('friends', {friends})
 })
