710649c6f5aa43bc0ed8a5f2d22a032d51d6fef0
Carmen Peralta <cperalta26@gmail.com>
2018-03-04 16:55:33 -0500

    console.log the time the sun sets in Hawaii

--- Colt-Steele/backEnd/APIs/first_request.js
+++ Colt-Steele/backEnd/APIs/first_request.js
@@ -1,10 +1,13 @@
 const request = require('request')
 
+console.log('Sunset in Hawaii is at...')
+
 request(
   'https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
   , (error, response, body) => {
     if (!error && response.statusCode === 200) {
-      console.log('body:', body) // Print the HTML for the Google homepage.
+      const parsedData = JSON.parse(body)
+      console.log(parsedData['query']['results']['channel']['astronomy']['sunset'])
     } else {
       console.log('SOMETHING WENT WRONG!')
       console.log('error:', error) // Print the error if one occurred
