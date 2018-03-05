fa795bf57d07f3fba793c2f4d7cf1a11f8c08d08
Carmen Peralta <cperalta26@gmail.com>
2018-03-05 18:44:51 -0500

    add search functionality

--- Colt-Steele/backEnd/APIs/movie_search_app/app.js
+++ Colt-Steele/backEnd/APIs/movie_search_app/app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const request = require('request')
+
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res, next) => {
+  res.render('search')
+})
+
 app.get('/results', (req, res, next) => {
-  request('http://www.omdbapi.com/?s=new+york&apikey=thewdb', (error, response, body) => {
+  const searchQuery = req.query.search
+  const url = `http://www.omdbapi.com/?s=${searchQuery}&apikey=thewdb`
+  request(url, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       const resultsData = JSON.parse(body)
       res.render('results', {resultsData})
