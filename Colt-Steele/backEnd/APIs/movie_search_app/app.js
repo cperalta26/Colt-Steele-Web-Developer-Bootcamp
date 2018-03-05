c3def9e963bfe0030af3b64d4d7fd5abe84b4128
Carmen Peralta <cperalta26@gmail.com>
2018-03-05 13:28:24 -0500

    hard code results route

---
+++ Colt-Steele/backEnd/APIs/movie_search_app/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const request = require('request')
+
+app.get('/results', (req, res, next) => {
+  request('http://www.omdbapi.com/?s=new+york&apikey=thewdb', (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const results = JSON.parse(body)
+      res.send(results['Search'][0]['Title'])
+      console.log('this is the body, ' + results)
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Movie App has started!!!')
+})
