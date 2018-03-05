05d2e33b817f315f10afd031783b628d82b493d6
Carmen Peralta <cperalta26@gmail.com>
2018-03-05 14:11:17 -0500

    add results template

--- Colt-Steele/backEnd/APIs/movie_search_app/app.js
+++ Colt-Steele/backEnd/APIs/movie_search_app/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const request = require('request')
+app.set('view engine', 'ejs')
 
 app.get('/results', (req, res, next) => {
   request('http://www.omdbapi.com/?s=new+york&apikey=thewdb', (error, response, body) => {
     if (!error && response.statusCode == 200) {
-      const results = JSON.parse(body)
-      res.send(results['Search'][0]['Title'])
+      const resultsData = JSON.parse(body)
+      res.render('results', {resultsData})
     }
   })
 })
