8b9c4cf8a5d81ec852a4b322d6c3fdb3e5c80b7d
Carmen Peralta <cperalta26@gmail.com>
2018-03-06 18:54:46 -0500

    add landing page template

--- Colt-Steele/yelpCamp/v1/app.js
+++ Colt-Steele/yelpCamp/v1/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res, next) => {
+  res.render('landing')
+})
+
 app.listen(3000, () => {
   console.log('The YelpCamp Server Has Started!')
 })
