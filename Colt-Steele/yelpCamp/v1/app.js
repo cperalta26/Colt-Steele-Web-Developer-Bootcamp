7bd58f08ded612cecda54a4d5a3a93c1893c61fb
Carmen Peralta <cperalta26@gmail.com>
2018-03-07 19:22:12 -0500

    add campgrounds template

--- Colt-Steele/yelpCamp/v1/app.js
+++ Colt-Steele/yelpCamp/v1/app.js
@@ -7,6 +7,16 @@ app.get('/', (req, res, next) => {
   res.render('landing')
 })
 
+app.get('/campgrounds', (req, res, next) => {
+  const campgrounds = [
+    {name: 'Salmon Creek', image: 'https://pixabay.com/get/e83db1082cfd043ed95c4518b7444795ea76e5d004b0144395f4c778a1ebb2_340.jpg'},
+    {name: 'Granite Hill', image: 'https://pixabay.com/get/e834b90621f6083ed95c4518b7444795ea76e5d004b0144395f4c77aa2ebb1_340.jpg'},
+    {name: "Mountain Goat's Rest", image: 'https://pixabay.com/get/eb31b00e28f2083ed95c4518b7444795ea76e5d004b0144395f4c771a7efbd_340.jpg'}
+  ]
+
+  res.render('campgrounds', {campgrounds})
+})
+
 app.listen(3000, () => {
   console.log('The YelpCamp Server Has Started!')
 })
