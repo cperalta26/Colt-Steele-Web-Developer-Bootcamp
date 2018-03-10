fadcac6797d54d64feac8121a9097a7b90f7c9d1
Carmen Peralta <cperalta26@gmail.com>
2018-03-10 18:59:42 -0500

    use db to get all campgrounds

--- Colt-Steele/yelpCamp/v2/app.js
+++ Colt-Steele/yelpCamp/v2/app.js
@@ -14,7 +14,7 @@ const campgroundSchema = new mongoose.Schema({
 
 const Campground = mongoose.model('Campground', campgroundSchema)
 
-Campground.create(
+/* Campground.create(
   {
     name: 'Granite Hill',
     image: 'https://pixabay.com/get/e834b90621f6083ed95c4518b7444795ea76e5d004b0144395f4c77aa2ebb1_340.jpg'
@@ -26,13 +26,7 @@ Campground.create(
       console.log('Newly created campground: ')
       console.log(campground)
     }
-  })
-
-const campgrounds = [
-  {name: 'Salmon Creek', image: 'https://pixabay.com/get/e83db1082cfd043ed95c4518b7444795ea76e5d004b0144395f4c778a1ebb2_340.jpg'},
-  {name: 'Granite Hill', image: 'https://pixabay.com/get/e834b90621f6083ed95c4518b7444795ea76e5d004b0144395f4c77aa2ebb1_340.jpg'},
-  {name: "Mountain Goat's Rest", image: 'https://pixabay.com/get/eb31b00e28f2083ed95c4518b7444795ea76e5d004b0144395f4c771a7efbd_340.jpg'}
-]
+  }) */
 
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -43,7 +37,14 @@ app.get('/', (req, res, next) => {
 })
 
 app.get('/campgrounds', (req, res, next) => {
-  res.render('campgrounds', {campgrounds})
+  //Get all campground from DB
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log('This is the error ' + err)
+    } else {
+      res.render('campgrounds', {allCampgrounds})
+    }
+  })
 })
 
 app.post('/campgrounds', (req, res, next) => {
