88c6e39fdea9ef76977f00c93d0b262ce3e94808
Carmen Peralta <cperalta26@gmail.com>
2018-03-10 15:43:06 -0500

    add mongoose

--- Colt-Steele/yelpCamp/v2/app.js
+++ Colt-Steele/yelpCamp/v2/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/yelp_camp')
+
 const campgrounds = [
   {name: 'Salmon Creek', image: 'https://pixabay.com/get/e83db1082cfd043ed95c4518b7444795ea76e5d004b0144395f4c778a1ebb2_340.jpg'},
   {name: 'Granite Hill', image: 'https://pixabay.com/get/e834b90621f6083ed95c4518b7444795ea76e5d004b0144395f4c77aa2ebb1_340.jpg'},
