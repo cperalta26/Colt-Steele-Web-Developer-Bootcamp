987853ca2bae935f3377b506f7c265cf71bdf8c8
Carmen Peralta <cperalta26@gmail.com>
2018-03-10 16:17:10 -0500

    add campground schema

--- Colt-Steele/yelpCamp/v2/app.js
+++ Colt-Steele/yelpCamp/v2/app.js
@@ -5,6 +5,29 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/yelp_camp')
 
+//Schema Setup
+
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+})
+
+const Campground = mongoose.model('Campground', campgroundSchema)
+
+Campground.create(
+  {
+    name: 'Granite Hill',
+    image: 'https://pixabay.com/get/e834b90621f6083ed95c4518b7444795ea76e5d004b0144395f4c77aa2ebb1_340.jpg'
+
+  }, (err, campground) => {
+    if (err) {
+      console.log('This is the error ' + err)
+    } else {
+      console.log('Newly created campground: ')
+      console.log(campground)
+    }
+  })
+
 const campgrounds = [
   {name: 'Salmon Creek', image: 'https://pixabay.com/get/e83db1082cfd043ed95c4518b7444795ea76e5d004b0144395f4c778a1ebb2_340.jpg'},
   {name: 'Granite Hill', image: 'https://pixabay.com/get/e834b90621f6083ed95c4518b7444795ea76e5d004b0144395f4c77aa2ebb1_340.jpg'},
