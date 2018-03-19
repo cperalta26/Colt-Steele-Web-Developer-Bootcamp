7c96e46649975d021ed395ba4d57ef4fc4f1b7a0
Carmen Peralta <cperalta26@gmail.com>
2018-03-19 16:40:22 -0400

    setup ejs

--- Colt-Steele/blogApp/app.js
+++ Colt-Steele/blogApp/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
