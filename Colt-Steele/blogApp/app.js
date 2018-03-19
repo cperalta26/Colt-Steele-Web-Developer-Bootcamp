632e3bf6a985c2fd6972aaa1a52753c7f89e1971
Carmen Peralta <cperalta26@gmail.com>
2018-03-19 16:49:15 -0400

    config db

--- Colt-Steele/blogApp/app.js
+++ Colt-Steele/blogApp/app.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
+//APP CONFIG
+mongoose.connect('mongodb://localhost/blogApp')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+
+//MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
