700e16a563de67f537c1f252924d7cf2f43c61c1
Carmen Peralta <cperalta26@gmail.com>
2018-03-20 20:13:06 -0400

    feat: create a new blog

--- Colt-Steele/blogApp/app.js
+++ Colt-Steele/blogApp/app.js
@@ -45,6 +45,19 @@ app.get('/blogs/new', (req, res) => {
   res.render('new')
 })
 
+//CREATE ROUTE
+app.post('/blogs', (req, res) => {
+  //create blog
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) {
+      res.render('new')
+    } else {
+      //then, redirect to the index
+      console.log('Newly created blog: ', newBlog)
+      res.redirect('/blogs')
+    }
+  })
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
