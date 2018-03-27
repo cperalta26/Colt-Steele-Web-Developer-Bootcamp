f93d5f714ed489ca0f1b8c19226e4490c57d8bc6
Carmen Peralta <cperalta26@gmail.com>
2018-03-27 17:51:58 -0400

    add association between a user and posts

--- Colt-Steele/backEnd/Associations/embed.js
+++ Colt-Steele/backEnd/Associations/embed.js
@@ -3,14 +3,6 @@ mongoose.connect('mongodb://localhost/blog_demo')
 
 //defining 2 models users and posts
 
-//USER - email, name
-const userSchema = new mongoose.Schema({
-  email: String,
-  name: String
-})
-
-const User = mongoose.model('User', userSchema)
-
 //POST - title, content
 const postSchema = new mongoose.Schema({
   title: String,
@@ -19,12 +11,26 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', postSchema)
 
-const newUser = new User({
-  email: 'charlie@brown.edu',
-  name: 'Charlie Brown'
+//USER - email, name
+const userSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  posts: [postSchema]
 })
 
-/* newUser.save((err, user) => {
+const User = mongoose.model('User', userSchema)
+
+/* const newUser = new User({
+  email: 'hermione@hogwarts.edu',
+  name: 'Hermione Granger'
+})
+
+newUser.posts.push({
+  title: 'How to bre polyjuice potion',
+  content: 'Just kidding. Go to potions class to learn it!'
+})
+
+newUser.save((err, user) => {
   if (err) {
     console.log(`This is the error: ${err}`)
   } else {
@@ -32,15 +38,31 @@ const newUser = new User({
   }
 }) */
 
-const newPost = new Post({
+/* const newPost = new Post({
   title: 'Reflections on Apples',
   content: 'They are delicios'
 })
 
-/* newPost.save((err, post) => {
+newPost.save((err, post) => {
   if (err) {
     console.log(`This is the error: ${err}`)
   } else {
     console.log(`This is the post that was just created ${post}`)
   }
 }) */
+
+User.findOne({name: 'Hermione Granger'}, (err, user) => {
+  if (err) {
+    console.log(`This is the error: ${err}`)
+  } else {
+    console.log(`The following user was found: ${user}`)
+    user.posts.push({
+      title: '3 things I really hate',
+      content: 'Voldemort. Voldemort. Voldemort.'
+    })
+    user.save((err, user) => {
+      if (err) console.log(`This is the error: ${err}`)
+      else console.log(`This is the updated user: ${user}`)
+    })
+  }
+})
