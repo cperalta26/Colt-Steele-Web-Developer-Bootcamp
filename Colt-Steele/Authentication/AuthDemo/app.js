8bfabb811d1e23572b32592be8862774c1bdb5da
Carmen Peralta <cperalta26@gmail.com>
2018-04-09 17:59:34 -0400

    add index route

--- Colt-Steele/Authentication/AuthDemo/app.js
+++ Colt-Steele/Authentication/AuthDemo/app.js
@@ -3,6 +3,11 @@ const app = express()
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/auth_demo_app')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
 
 app.listen(3000, () => {
   console.log('Now listening on port 3000!!!')
