4d3c735324696370c888cb27180efd4d3631e9d5
Carmen Peralta <cperalta26@gmail.com>
2018-04-12 18:35:22 -0400

    configure passport

--- Colt-Steele/yelpCamp/v6/app.js
+++ Colt-Steele/yelpCamp/v6/app.js
@@ -15,6 +15,19 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static(`${__dirname}/public`))
 
+//PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: 'Rusty is the cutest',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.get('/', (req, res, next) => {
   res.render('landing')
 })
