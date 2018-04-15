3d0d80577114797c79d8d67d14af4f29689f71f7
Carmen Peralta <cperalta26@gmail.com>
2018-04-15 15:30:33 -0400

    v9: add delete route and form

--- Colt-Steele/yelpCamp/v9/routes/campgrounds.js
+++ Colt-Steele/yelpCamp/v9/routes/campgrounds.js
@@ -80,6 +80,18 @@ router.put('/:id', (req, res) => {
   })
 })
 
+//DESTROY CAMPGROUND ROUTE
+router.delete('/:id', (req, res) => {
+  Campground.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      console.log(`Found the following error when trying to delete: ${err}`)
+      res.redirect('/campgrounds')
+    } else {
+      res.redirect('/campgrounds')
+    }
+  })
+})
+
 function isLoggedIn(req, res, next){
   if (req.isAuthenticated()){
     return next()
