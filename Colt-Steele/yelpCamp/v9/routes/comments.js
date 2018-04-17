79030b8dad117a2a66ffa6652f6015538fe285d5
Carmen Peralta <cperalta26@gmail.com>
2018-04-17 18:34:55 -0400

    v9: add edit and update routes for comments

--- Colt-Steele/yelpCamp/v9/routes/comments.js
+++ Colt-Steele/yelpCamp/v9/routes/comments.js
@@ -43,6 +43,30 @@ router.post('/', isLoggedIn, (req, res) => {
   })
 })
 
+//COMMENT EDIT ROUTE
+router.get('/:comment_id/edit', (req, res) => {
+  Comment.findById(req.params.comment_id, (err, comment) => {
+    if (err) {
+      console.log(`Found the following error when trying to edit a comment: ${err}`)
+      res.redirect('back')
+    } else {
+      res.render('comments/edit', {campgroundId: req.params.id, comment})
+    }
+  })
+})
+
+//COMMENT UPDATE ROUTE
+router.put('/:comment_id', (req, res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+    if (err) {
+      res.redirect('back')
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`)
+    }
+  })
+})
+
+//middleware
 function isLoggedIn(req, res, next){
   if (req.isAuthenticated()){
     return next()
